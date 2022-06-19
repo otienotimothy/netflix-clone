b40e784c54b88780e017843fa3455614eba9b87c
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-06-19 18:56:53 +0300

    Add and set up react-query. A data syncronization tool

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { QueryClientProvider, QueryClient } from "react-query";
 // Components Import
 import { Navbar } from "./components";
 
 // Pages Import
-import {Home} from './pages'
+import { Home } from "./pages";
 
 function App() {
+	const queryClient = new QueryClient();
+
 	return (
 		<Router>
-			<Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+			<QueryClientProvider client={queryClient}>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</QueryClientProvider>
 		</Router>
 	);
 }
