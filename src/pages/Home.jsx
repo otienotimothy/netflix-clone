b37e586dd3251066ad957a65b8edb28f51ae2e77
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-06-20 11:12:58 +0300

    Add the row components to the different movie types and genres

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
-import {Hero} from '../components'
+import {Hero, Row} from '../components'
+import {URL_PATHS} from '../api/urls'
 
 export const Home = () => {
+
+  const { upcoming, popular, trending, topRated, horror } = URL_PATHS
+
   return (
     <div>
       <Hero />
+      <Row title={'Trending'} url={trending} />
+      <Row title={'Popular'} url={popular} />
+      <Row title={'Upcoming'} url={upcoming} />
+      <Row title={'Top Rated'} url={topRated} />
+      <Row title={'Horror'} url={horror} />
     </div>
   )
 }
