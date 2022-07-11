dad3d1e9d34f6574590b59792bde977df5719745
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-07-11 11:50:05 +0300

    feat: export jumbo component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
-export {Navbar} from './navbar/Navbar'
-export {Hero} from './hero/Hero'
-export {Row} from './row/Row'
+export {Navbar} from './navbar/Navbar';
+export {Hero} from './hero/Hero';
+export {Row} from './row/Row';
+export {Jumbo} from './jumbotron/Jumbo';
