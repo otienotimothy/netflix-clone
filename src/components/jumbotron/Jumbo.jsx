cb26b6b71fd8445d43bc468d14be8a9f0d2d6bb9
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-07-27 14:43:16 +0300

    feat:modify details page styling

--- src/components/jumbotron/Jumbo.jsx
+++ src/components/jumbotron/Jumbo.jsx
@@ -3,10 +3,10 @@ export const Jumbo = ({ children, direction = "row" }) => {
 		<>
 			<div
 				style={{ borderBottom: "8px solid #222" }}
-				className="flex py-[50px] px-[5%] text-white overflow-hidden">
+				className="flex py-[60px] px-[5%] text-white overflow-hidden">
 				<div
-					className={`flex flex-${direction} items-center justify-between max-w-[1100px] w-full lg:flex-col`}>
-					<div className="">{children}</div>
+					className={`flex ${direction} flex-wrap items-center justify-center md:justify-between max-w-[1100px] w-full`}>
+					{children}
 				</div>
 			</div>
 		</>
@@ -18,15 +18,15 @@ Jumbo.Container = function({ children }) {
 };
 
 Jumbo.Pane = function({ children }) {
-	return <div className="w-1/2 lg:w-full lg:text-center lg:py-0 lg:px-[45px]" >{children}</div>;
+	return <div className="w-1/2 min-w-[320px] text-center lg:py-0 lg:px-[45px]" >{children}</div>;
 };
 
 Jumbo.Title = function({children}) {
-    return <h1 className="text-[50px] leading-[1.1] mb-2 sm:text-[35px] " > {children} </h1>
+    return <h1 className="text-[30px] md:text-[40px] lg:text-[50px] leading-[1.1] mb-2" > {children} </h1>
 }
 
 Jumbo.SubTitle = function({children}) {
-    return <h2 className="text-[26px] font-normal leading-[normal]" > {children} </h2>
+    return <h2 className="text-[20px] md:text-[26px] lg:text-[30px] font-normal leading-[normal]" > {children} </h2>
 }
 
 Jumbo.Image = function({src, alt}){
