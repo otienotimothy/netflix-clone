13ade99a8d2d71c5729115468711119553434ee8
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-07-27 14:41:55 +0300

    modify seed data

--- src/seed.js
+++ src/seed.js
@@ -1,46 +1,55 @@
 export const faqs = [
-    {
-      "header": "What is Netflix?",
-      "body": "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices.\n\nYou can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!"
-    },
-    {
-      "header": "How much does Netflix cost?",
-      "body": "Watch Netflix on your smartphone, tablet, smart TV, laptop or streaming device, all for one low fixed monthly fee. Plans start from £5.99 a month. No extra costs or contracts."
-    },
-    {
-      "header": "Where can I watch?",
-      "body": "Watch anywhere, anytime, on an unlimited number of devices. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.\n\nYou can also download your favourite programmes with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere."
-    },
-    {
-      "header": "How do I cancel?",
-      "body": "Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account at any time."
-    },
-    {
-      "header": "What can I watch on Netflix?",
-      "body": "Netflix has an extensive library of feature films, documentaries, TV programmes, anime, award-winning Netflix originals, and more. Watch as much as you want, any time you want."
-    }
-  ]
-
+	{
+		id: 1,
+		header: "What is Netflix?",
+		body: "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices.\n\nYou can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!"
+	},
+	{
+		id: 2,
+		header: "How much does Netflix cost?",
+		body: "Watch Netflix on your smartphone, tablet, smart TV, laptop or streaming device, all for one low fixed monthly fee. Plans start from £5.99 a month. No extra costs or contracts."
+	},
+	{
+		id: 3,
+		header: "Where can I watch?",
+		body: "Watch anywhere, anytime, on an unlimited number of devices. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.\n\nYou can also download your favourite programmes with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere."
+	},
+	{
+		id: 4,
+		header: "How do I cancel?",
+		body: "Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account at any time."
+	},
+	{
+		id: 5,
+		header: "What can I watch on Netflix?",
+		body: "Netflix has an extensive library of feature films, documentaries, TV programmes, anime, award-winning Netflix originals, and more. Watch as much as you want, any time you want."
+	}
+];
 export const jumbo = [
-    {
-      "title": "Enjoy on your TV.",
-      "subTitle": "Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.",
-      "image": "/images/misc/home-tv.jpg",
-      "alt": "Tiger King on Netflix",
-      "direction": "row"
-    },
-    {
-      "title": "Download your programmes to watch on the go.",
-      "subTitle": "Save your data and watch all your favourites offline.",
-      "image": "/images/misc/home-mobile.jpg",
-      "alt": "Watch on mobile",
-      "direction": "row-reverse"
-    },
-    {
-      "title": "Watch everywhere.",
-      "subTitle": "Stream unlimited films and TV programmes on your phone, tablet, laptop and TV without paying more.",
-      "image": "/images/misc/home-imac.jpg",
-      "alt": "Money Heist on Netflix",
-      "direction": "row"
-    }
-  ]
+	{
+		id: 1,
+		title: "Enjoy on your TV.",
+		subTitle:
+			"Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.",
+		image: "/images/misc/home-tv.jpg",
+		alt: "Tiger King on Netflix",
+		direction: "flex-row"
+	},
+	{
+		id: 2,
+		title: "Download your programmes to watch on the go.",
+		subTitle: "Save your data and watch all your favourites offline.",
+		image: "/images/misc/home-mobile.jpg",
+		alt: "Watch on mobile",
+		direction: "flex-row-reverse"
+	},
+	{
+		id: 3,
+		title: "Watch everywhere.",
+		subTitle:
+			"Stream unlimited films and TV programmes on your phone, tablet, laptop and TV without paying more.",
+		image: "/images/misc/home-imac.jpg",
+		alt: "Money Heist on Netflix",
+		direction: "flex-row"
+	}
+];
